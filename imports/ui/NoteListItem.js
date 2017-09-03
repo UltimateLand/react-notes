e4aa6a462b5ff1cfc9e8adb402eea18772547e16
Jun-Ren, Chen <UltimateLand@hotmail.com>
2017-09-03 14:15:31 +0800

    Mark selected note in list

--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -9,6 +9,7 @@ export const NoteListItem = (props) => {
   return (
     <div onClick={() => { props.Session.set('selectedNoteId', props.note._id)}}>
       <h5>{ props.note.title || 'Untitled note' }</h5>
+      { props.note.selected ? 'Selectd' : undefined }
       <p>{ moment(props.note.updatedAt).format('M/DD/YY') }</p>
       <button onClick={() => { Meteor.call('notes.remove', props.note._id) }}>Delete</button>
     </div>
